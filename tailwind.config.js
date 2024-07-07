0645623e12e7f5d6c39d7ffa197a24535684b3b7
sandalidilshani <79295251+sandalidilshani@users.noreply.github.com>
2024-07-07 22:58:48 +0530

    complete
    complete

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend:{ 
+      colors: {
+      'pink':'#eeddcc',
+      'darkpink':'#D3A17E',
+      'brown':'#34251f',
+      'brown-bg':'#413531',
+      'light-pink':'#faf5f0'
+
+
+    },
+  }
+   
+  },
+  plugins: [],
+}
+
